1325bc9ce6439f6e82fd7a009b0951a01a9c5519
Aman Saharan <amansaharan2013@gmail.com>
2020-05-20 06:02:16 +0530

    Updates

---
+++ precache-manifest.7345fadef37009f5bc211ebc5b2393e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f652cb677709f4a6eca1171a3b2fe74",
+    "url": "/pokegame/index.html"
+  },
+  {
+    "revision": "d552891d6b5343828d5f",
+    "url": "/pokegame/static/css/main.c2f0ab2d.chunk.css"
+  },
+  {
+    "revision": "c92c6b257b3b0b785dc3",
+    "url": "/pokegame/static/js/2.fb520a37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokegame/static/js/2.fb520a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d552891d6b5343828d5f",
+    "url": "/pokegame/static/js/main.9b81ddae.chunk.js"
+  },
+  {
+    "revision": "6d1106e5eda2ef9a3928",
+    "url": "/pokegame/static/js/runtime-main.579f8ae2.js"
+  }
+]);
